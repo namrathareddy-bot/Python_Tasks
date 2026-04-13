d28565b735414f63143828c205cdb5d11901685d
Namratha <namrathareddy816@gmail.com>
2026-04-13 09:57:19 +0530

    python tasks

---
+++ Day13_Tasks/countoccrences.py
@@ -0,0 +1,6 @@
+import numpy as np
+data=np.array([1,2,2,3,1,4,2,3])
+unique , count=np.unique(data,return_counts=True)
+print("Numbers:",unique)
+print("Counts:",count)
+print(dict(zip(unique,count)))
